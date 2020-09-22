a434ffbfe5d055dd38343d23e20738429ba1fff4
Nqvinh04 <nqvinh1995@gmail.com>
2020-09-22 23:00:16 +0700

    BT

---
+++ src/main/java/com/codegym/bt/model/User.java
@@ -0,0 +1,68 @@
+package com.codegym.bt.model;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.*;
+
+@Component
+public class User {
+
+    @NotEmpty
+    @Size(min = 5, max = 45)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 5, max = 45)
+    private String lastName;
+
+    @Pattern(regexp="^[0-9]{10}")
+    private String phoneNumber;
+
+    @Min(18)
+    private int age;
+
+    @Email
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/codegym/bt/service/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.codegym.bt.service;
+
+public class UserServiceImpl {
+}
